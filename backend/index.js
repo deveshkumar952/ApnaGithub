fd74ea01d01e47cdcd614b899ae20dfcef554b9b
Devesh Kumar <Deveshkumar952@gmail.com>
2026-01-15 23:06:37 +0530

    server running

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import http from "http";
+import {Server} from "socket.io";
+
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 import { initRepo } from "./controllers/init.controller.js";
 import { addRepo } from "./controllers/add.controller.js";
-import {commitRepo} from "./controllers/commit.controller.js"
-import {pushRepo} from "./controllers/push.controller.js"
-import {pullRepo} from "./controllers/pull.controller.js"
-import {revertRepo} from "./controllers/revert.controller.js"
+import { commitRepo } from "./controllers/commit.controller.js";
+import { pushRepo } from "./controllers/push.controller.js";
+import { pullRepo } from "./controllers/pull.controller.js";
+import { revertRepo } from "./controllers/revert.controller.js";
+
+
+dotenv.config();
 
 yargs(hideBin(process.argv))
+  .command("start", "Starts a new server", {}, startServer)
   .command("init", "Initialise a new repository", {}, initRepo)
   .command(
     "add <file>",
@@ -53,3 +65,55 @@ yargs(hideBin(process.argv))
   )
   .demandCommand(1, "You need at least one command")
   .help().argv;
+
+
+  function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURI = process.env.MONGODB_URI;
+
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => console.error("Unable to connect : ", err));
+
+  app.use(cors({ origin: "*" }));
+
+  app.use("/", (req,res)=>{
+    res.send("welcome");
+  });
+
+  let user = "test";
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("joinRoom", (userID) => {
+      user = userID;
+      console.log("=====");
+      console.log(user);
+      console.log("=====");
+      socket.join(userID);
+    });
+  });
+
+  const db = mongoose.connection;
+
+  db.once("open", async () => {
+    console.log("CRUD operations called");
+    // CRUD operations
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`);
+  });
+}
